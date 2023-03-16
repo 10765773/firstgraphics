223a88af6e5587b08d94b73a4449fc307e92b515
Jakub Pazdan <10765773@cityplym.ac.uk>
2023-03-16 16:36:17 +0000

    init abstractclass

--- main.cpp
+++ main.cpp
@@ -1,8 +1,25 @@
 #include <SDL.h>
 
+class AbstractCanvas{
+private:
+    SDL_Renderer *sdlRenderer;
+
+public:
+    AbstractCanvas(SDL_Renderer *aSdlRenderer) {
+        this->sdlRenderer = aSdlRenderer;
+    }
+
+    void setBgColor(int r, int g, int b){
+        SDL_SetRenderDrawColor(sdlRenderer, r, g, b, SDL_ALPHA_OPAQUE);
+        SDL_RenderClear(sdlRenderer);
+        SDL_RenderPresent(sdlRenderer);
+    }
+};
+
 int main(int argv, char** args){
     bool quit;
     SDL_Event event;
+
     // SDL setup
     SDL_Init(SDL_INIT_VIDEO);
     SDL_Window *window = SDL_CreateWindow("First Graphics",
@@ -12,6 +29,8 @@ int main(int argv, char** args){
                                           720,
                                           0);
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
+
+    AbstractCanvas canvas(renderer);
 
     //Main loop
     while(!quit){
@@ -24,9 +43,9 @@ int main(int argv, char** args){
                 break;
         }
         SDL_SetRenderDrawColor(renderer,255,255,255,SDL_ALPHA_OPAQUE);
-        SDL_RenderClear(renderer);
-
-        SDL_RenderPresent(renderer);
+        //SDL_RenderClear(renderer);
+        canvas.setBgColor(255,255,0);
+        //SDL_RenderPresent(renderer);
     }
     //SDL cleanup
     SDL_DestroyRenderer(renderer);
